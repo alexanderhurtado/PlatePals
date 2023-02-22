d16c963233ccd1225052098ee15465283ccce552
Alexander Hurtado <Alexander.Hurtado@southeasttech.edu>
2023-02-21 23:26:19 -0600

    about us paragraph

--- about.php
+++ about.php
@@ -1,9 +1,11 @@
 <?php include 'view/header.php'; ?>
 <body>
 	<div class="About">
-		<h2>This is information</h2>
-		<p>About this application. Instructions and whatnot can
-			be entered in this paragraph element.</p>
+		<h2>About Us</h2>
+		<p> Our dedicated team of developers are passionate about helping you and your loved ones decide
+            on an agreed restaurant to eat at. Since our founding in 2023, PlatesPals has enabled 20 
+            couples/families around Sioux Falls to find the perfect dining choice. whether you are a couple
+            going out on a date or a group of friends just looking for a good meal, PlatePals is here for you.</p>
 	</div>
 </body>
 <?php include 'view/footer.php'; ?>
