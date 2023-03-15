a0541263855e039ee40d1cb2e01066de1a3cb381
Nick VerMulm <37892064+marcello360@users.noreply.github.com>
2023-03-15 13:16:48 -0500

    Updated login redirect

--- user/login.php
+++ user/login.php
@@ -15,11 +15,17 @@ if (isset($_POST['username'])){
 	if($rows==1){
 		$_SESSION['username'] = $username;
 		header("Location: ../index.php");
-	}else{
-		//Should implement error with the form included
-		echo "<div class='form'>
-			<h3>Username/password is incorrect.</h3>
-			<br/>Click here to <a href='login.php'>Login</a></div><br/>";
+	}else{ ?>
+		<div class="form">
+			<h3>Username/Password is incorrect</h3>
+			<h1>Log In</h1>
+			<form action="" method="post" name="login">
+				<input type="text" name="username" placeholder="Username" required autofocus />
+				<input type="password" name="password" placeholder="Password" required />
+				<input name="submit" type="submit" value="Login" />
+			</form>
+			<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
+		</div> <?php
 	}
 }else{ ?>
 	<div class="form">
