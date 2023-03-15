67dbcc555d39efee6b66649f9da5b8b8d1c29902
Alexander Hurtado <Alexander.Hurtado@southeasttech.edu>
2023-03-15 13:18:16 -0500

    commented out the wrong code

--- matches/index.php
+++ matches/index.php
@@ -10,16 +10,15 @@
 	$restaurants = $userDB->getFavorites();
 	
 	//Add if/else statement to include error page if favorites is empty
-  public static function getName () {
-    if (!isset(self::$db)) {
-        try {
-            $restaurant->getName();
-        } catch (PDOException $e) {
-            $error_message = $e->getMessage();
-            include('../errors/matches_error.php');
-            exit();
-        }
-    }
-  
-	include('matches_view.php');
+    //public static function getFavorites() {
+    //    if (!isset(self::$db)) {
+    //        try {
+    //            $restaurant->getName();
+    //        } catch (PDOException $e) {
+    //            $error_message = $e->getMessage();
+    //            include('../errors/matches_error.php');
+    //        }
+    //    }
+    //    return('matches_view.php');
+    //}
 ?>
