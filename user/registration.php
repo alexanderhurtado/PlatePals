b98baaa6389583648f76969536d7457c43c92250
Nick VerMulm <37892064+marcello360@users.noreply.github.com>
2023-03-28 08:46:11 -0500

    Updated registration requirements

--- user/registration.php
+++ user/registration.php
@@ -16,31 +16,34 @@ if (isset($_REQUEST['username'])){
 	$uppercase = preg_match('@[A-Z]@', $password);
 	$lowercase = preg_match('@[a-z]@', $password);
 	$number    = preg_match('@[0-9]@', $password);
-
 	
-	$check = $userDB->checkUser($username);
-	if ($check) { ?>
-		<p style="color:#FF0000" class="first_paragraph">Username already taken. Please try again.</p>
-		<?php include '../view/register_form.html';
-	} else if (!$uppercase || !$lowercase || !$number || mb_strlen($password) < 8) { ?>
-		<p style="color:#FF0000" class="first_paragraph">Password should be at least 8 characters
-		in length and should include at least one upper-case letter and one number.</p>
-		<?php include '../view/register_form.html';
+	if (!ctype_alnum($username)) { ?>
+		<p style="color:#FF0000" class="first_paragraph">Username should only contain only letters and numbers.</p>
+		<?php include '../view/register_form.html'; 
 	} else {
-		$result = $userDB->addUser($username, $password, $trn_date);
-		if($result){
-			$userDB->createList($username);
-			echo "<div class='form'>
-				<h3>You are registered successfully.</h3>
-				<br/>Click here to <a href='login.php'>Login</a></div>";
+		$check = $userDB->checkUser($username);
+		if ($check) { ?>
+			<p style="color:#FF0000" class="first_paragraph">Username already taken. Please try again.</p>
+			<?php include '../view/register_form.html';
+		} else if (!$uppercase || !$lowercase || !$number || mb_strlen($password) < 8) { ?>
+			<p style="color:#FF0000" class="first_paragraph">Password should be at least 8 characters
+			in length and should include at least one upper-case letter and one number.</p>
+			<?php include '../view/register_form.html';
+		} else {
+			$result = $userDB->addUser($username, $password, $trn_date);
+			if($result){
+				$userDB->createList($username);
+				echo "<div class='form'>
+					<h3>You are registered successfully.</h3>
+					<br/>Click here to <a href='login.php'>Login</a></div>";
+			}
 		}
 	}
 }else{ 
 	include '../view/register_form.html'; ?>
-	<br>
+<?php } ?>
 	<h4>Registration Requirements</h4>
-		<p> Password must be in between 1 and 10 characters. </p>
-		<p> Password must not contain special characters, such as !@#$%&*()_+.</p>
-		<p> Username must be unique.</p>
-<?php }
-include '../view/footer.php'; ?>
+		<p> Username cannot contain any special characters - only letters and numbers.</p>
+		<p> Password must be at least 8 characters in length. </p>
+		<p> Password must contain at least 1 capital letter and 1 number.</p>
+<?php include '../view/footer.php'; ?>
