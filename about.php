f59bce13a52ed84b6141927bd087b5c2b3a53a55
Alexander Hurtado <Alexander.Hurtado@southeasttech.edu>
2023-04-04 11:04:33 -0500

    little update to about us paragraph

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
 			What do you do? this was the problem everytime when my significant other and I would try to decide on a
 			place to dine at. what about this, what about that, it went on and on. in 2023, I was introduced to Nick who had this
 			idea of an web app that would help you find a place to eat that both parties would enjoy. as soon as I heard that,
-			on board.<p><br>
+			I was on board.<p><br>
 		<p>Our dedicated team of developers are passionate about helping you and your loved ones decide
 			on an agreed restaurant to eat at. Since our founding in 2023, PlatesPals has enabled 20 
 			couples/families around Sioux Falls to find the perfect dining choice. whether you are a couple
